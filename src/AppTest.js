ce1604c8d84ad54d0d1abda7f3d75105d31f22e3
sherin-ranjitha <sherinranjitha22@gmail.com>
2023-10-13 23:30:24 +0530

    testing added

---
+++ src/AppTest.js
@@ -0,0 +1,16 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+
+test('Renders the Header heading', () => {
+    render(<BrowserRouter><App /></BrowserRouter>);
+    const headingElement = screen.getByText("Reserve Table");
+    expect(headingElement).toBeInTheDocument();
+
+    const reserveButton = screen.getByRole("button");
+    fireEvent.click(reserveButton);
+
+    const headingElementNew = screen.getByText("Choose Date");
+    expect(headingElementNew).toBeInTheDocument();
+})
+
